3b0b324a231cb166473bdcf32ed997b991434913
Sayan Mondal <101855586+Sayan-sss@users.noreply.github.com>
2024-01-06 10:54:35 +0530

    stripe webhook endpoint updated

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -2,9 +2,9 @@
 
 import { IEvent } from "@/lib/database/models/event.model";
 import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
-import Link from "next/link";
 import Checkout from "./Checkout";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
@@ -13,7 +13,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
   const hasEventFinished = new Date(event.endDateTime) < new Date();
 
   return (
-    <div className=" flex items-center gap-3">
+    <div className="flex items-center gap-3">
       {hasEventFinished ? (
         <p className="p-2 text-red-400">
           Sorry, tickets are no longer available.
@@ -21,8 +21,8 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
       ) : (
         <>
           <SignedOut>
-            <Button className="button rounded-full" size="lg">
-              <Link href="/sign-in">Get Ticket</Link>
+            <Button asChild className="button rounded-full" size="lg">
+              <Link href="/sign-in">Get Tickets</Link>
             </Button>
           </SignedOut>
 
